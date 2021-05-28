89fdd0d83e673c9f76e62921c6f765f230e4bbdc
Felipe <compilerman@felipe>
2021-05-28 00:23:07 -0300

    Bom dia

---
+++ ProjetoTeste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Bom dia");
+            Console.WriteLine("Como vc está");
+            Console.WriteLine();
+        }
+    }
+}
